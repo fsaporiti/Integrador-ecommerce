87bc499480feea058fb571a932ba736f9c247aaf
facundo <facundo.saporiti@hotmail.com>
2023-07-14 16:30:18 -0300

    Datos de BD corregidos, archivos agregados anteriormente Eliminados

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = 
 {
   "development": {
-    "username": "process.env.DB_USER",
+    "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
